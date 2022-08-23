d7682461556cac2b54101df22d961cb6a1e81ab0
Abraham Peter <47416051+theclassical-dev@users.noreply.github.com>
2022-08-23 18:58:13 +0100

    divided design implemented

--- lib/screens/tick_view.dart
+++ lib/screens/tick_view.dart
@@ -17,11 +17,15 @@ class TickView extends StatelessWidget {
       child: Container(
         margin: const EdgeInsets.only(left: 16),
         child: Column(
+          mainAxisSize: MainAxisSize.min,
           children: [
+            /*
+            *showing the blue part of the card
+            * */
             Container(
-              decoration: BoxDecoration(
-                color: const Color(0xFF526799),
-                borderRadius: const BorderRadius.only(topLeft: Radius.circular(21))
+              decoration: const BoxDecoration(
+                color: Color(0xFF526799),
+                borderRadius: BorderRadius.only(topLeft: Radius.circular(21))
               ),
               padding: const EdgeInsets.all(16),
               child: Column(
@@ -72,6 +76,7 @@ class TickView extends StatelessWidget {
                   Row(
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
+
                       SizedBox(
                         width: 100, child: Text("New-City", style: Styles.headLineStyle4.copyWith(color: Colors.white),),
                       ),
@@ -81,6 +86,65 @@ class TickView extends StatelessWidget {
                       ),
                     ],
                   )
+                ],
+              ),
+            ),
+            /*
+            *showing the orange part of the card
+            * */
+            Container(
+              color: Styles.orangeColor, //const Color(0xFFF37B67),
+              child: Row(
+                children: [
+                  const SizedBox(
+                    height: 20,
+                    width: 10,
+                    child: DecoratedBox(
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.only(
+                            topRight: Radius.circular(10),
+                            bottomRight: Radius.circular(10)
+                        )
+                      ),
+
+                    ),
+                  ),
+                  Expanded(child: Padding(
+                    padding: const EdgeInsets.all(12.0),
+                    child: LayoutBuilder(
+                      builder: (BuildContext context, BoxConstraints constraints) {
+                        return Flex(
+                          direction: Axis.horizontal,
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          mainAxisSize: MainAxisSize.max,
+                          children:  List.generate((constraints.constrainWidth()/15).floor(), (index) => const SizedBox(
+                            width: 5, height: 1,
+                            child: DecoratedBox(
+                              decoration: BoxDecoration(
+                                  color: Colors.white
+                              ),
+                            ),
+                          )),
+                        );
+                      },
+
+                    ),
+                  )),
+                   const SizedBox(
+                    height: 20,
+                    width: 10,
+                    child: DecoratedBox(
+                      decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.only(
+                              topLeft: Radius.circular(10),
+                              bottomLeft: Radius.circular(10)
+                          )
+                      ),
+
+                    ),
+                  ),
                 ],
               ),
             )
