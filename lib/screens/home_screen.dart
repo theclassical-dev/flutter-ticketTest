e211b228fa8edad93df394391489ce9bd86799c7
Abraham Peter <47416051+theclassical-dev@users.noreply.github.com>
2022-08-23 08:30:26 +0100

    image folder and setting done + first top row done

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,10 +1,50 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: Color(0xFFeeedf2),
+      body: ListView(
+        children: [
+          Container(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: Column(
+              children: [
+                // first row
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                   Column(
+                     crossAxisAlignment: CrossAxisAlignment.start,
+                     children: const [
+                       Text("Hello"),
+                       Text("TickT"),
+                     ],
+                   ),
+                    Container(
+                      height: 50,
+                      width: 50,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        image: const DecorationImage(
+                          fit: BoxFit.fitHeight,
+                          image: AssetImage(
+                              "assets/images/img_1.png"
+                          )
+                        )
+                      ),
+                    )
+                  ],
+                )
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
