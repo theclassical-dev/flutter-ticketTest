3f14f8e70becf31adb80d5fe1d785827ad3f7f55
Abraham Peter <47416051+theclassical-dev@users.noreply.github.com>
2022-08-24 10:50:16 +0100

    searchscreen update with  second container last part with a positioned stacked

--- lib/screens/search_screen.dart
+++ lib/screens/search_screen.dart
@@ -81,9 +81,10 @@ class SearchScreen extends StatelessWidget {
           /* upcoming flights*/
           Gap(AppLayout.getHeight(40)),
           const AppDoubleTextWidget(bigText: "Upcoming Fights", smallText: "view all"),
-          /**/
+          /* last container*/
           Gap(AppLayout.getHeight(15)),
           Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
               Container(
                 height: AppLayout.getHeight(350),
@@ -122,6 +123,47 @@ class SearchScreen extends StatelessWidget {
                   ],
                 ),
               ),
+              Column(
+                children: [
+                 Stack(
+                   children: [
+                     Container(
+                       width: size.width*0.44,
+                       height: AppLayout.getHeight(174),
+                       decoration: BoxDecoration(
+                           color: Color(0xFF3Ab8b8),
+                           borderRadius: BorderRadius.circular(AppLayout.getHeight(18))
+                       ),
+                       padding: EdgeInsets.symmetric(vertical: AppLayout.getHeight(15), horizontal: AppLayout.getWidth(15)),
+                       child: Column(
+                         crossAxisAlignment: CrossAxisAlignment.start,
+                         children: [
+                           Text("Discount\nfor survey", style: Styles.headLineStyle2.copyWith(fontWeight: FontWeight.bold,color: Colors.white ),),
+                           Gap(AppLayout.getHeight(10)),
+                           Text("Take the survey about our services and get discount.", style: Styles.headLineStyle2.copyWith(fontWeight: FontWeight.w500,color: Colors.white, fontSize: 18 ),),
+
+                         ],
+                       ),
+                     ),
+                    Positioned(
+                        right: -45,
+                        top: -40,
+                        child:  Container(
+                      padding: EdgeInsets.all(AppLayout.getHeight(30) ),
+                      decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          border: Border.all(width: 18, color: const Color(0xFF189999)),
+                          color: Colors.transparent
+                      ),
+                    )
+                    ),
+                   ],
+                 ),
+                  Container(
+                    
+                  )
+                ],
+              )
             ],
           )
         ],
