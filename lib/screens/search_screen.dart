7f8b03063fbb42c7c31d277c13f2164d6d4c640a
Abraham Peter <47416051+theclassical-dev@users.noreply.github.com>
2022-08-24 09:35:16 +0100

    searchscreen update with  departure container

--- lib/screens/search_screen.dart
+++ lib/screens/search_screen.dart
@@ -17,8 +17,8 @@ class SearchScreen extends StatelessWidget {
         children: [
           Gap(AppLayout.getHeight(40)),
           Text("What are \n you looking for ?", style: Styles.headLineStyle.copyWith(fontSize: 35),),
-          Gap(AppLayout.getHeight(40)),
-         FittedBox(
+          Gap(AppLayout.getHeight(20)),
+          FittedBox(
            child:  Container(
              padding: const EdgeInsets.all(3.5),
              child: Row(
@@ -59,8 +59,23 @@ class SearchScreen extends StatelessWidget {
 
              ),
            ),
-         )
-
+         ),
+          /* second  container*/
+          Gap(AppLayout.getHeight(25)),
+          Container(
+            padding: EdgeInsets.symmetric(horizontal: AppLayout.getWidth(12), vertical: AppLayout.getHeight(12)),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius:  BorderRadius.circular(AppLayout.getWidth(10),)
+            ),
+            child: Row(
+              children: [
+                const Icon(Icons.flight_takeoff_rounded, color: Color(0xFFBFC2DF),),
+                Gap(AppLayout.getWidth(10)),
+                Text("Departure")
+              ],
+            ),
+          ),
         ],
       ),
     );
